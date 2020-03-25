2d676309ce23e5f2b8071bffea163ae69e594d6c
BEUMGOO <captain_kbg@naver.com>
2020-03-25 23:52:22 +0900

    setting graphql

---
+++ src/server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+import { GraphQLServer } from "graphql-yoga";
+import schema from "./schema"
+
+const PORT = process.env.PORT || 4000;
+
+const server = new GraphQLServer({schema});
+
+server.start({port: PORT}, ()=>console.log(`server running on port ${PORT}`));
