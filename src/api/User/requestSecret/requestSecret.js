2d9fecd68899d565729f0fc23608a903df214f52
BEUMGOO <captain_kbg@naver.com>
2020-03-29 22:14:57 +0900

    send mail / create userSecret

---
+++ src/api/User/requestSecret/requestSecret.js
@@ -0,0 +1,23 @@
+import { generatorSecret } from "../../../utils";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    requestSecret: async (_, args) => {
+      const { email } = args;
+      const loginSecret = generatorSecret();
+      console.log(loginSecret);
+      try {
+        await prisma.updateUser({
+          data:{
+            loginSecret
+          },
+          where:{email}
+        })
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
+};
