ce5b49c6029a5b85dd633c674127692b91655d66
BEUMGOO <captain_kbg@naver.com>
2020-04-02 23:23:06 +0900

    toggleLike Resolver

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import "./env";
 import { GraphQLServer } from "graphql-yoga";
-import passport from "passport";
 import logger from "morgan";
 import schema from "./schema";
 import "./passport";
@@ -8,7 +7,10 @@ import { authenticateJwt } from "./passport";
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({schema});
+const server = new GraphQLServer({
+  schema,
+  context: ({ request }) => ({ request })
+});
 
 server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
