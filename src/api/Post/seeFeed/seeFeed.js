c47b796f0da142c1557f719adf3d360f6961ea90
BEUMGOO <captain_kbg@naver.com>
2020-04-14 23:55:10 +0900

    seeFeed resolver

---
+++ src/api/Post/seeFeed/seeFeed.js
@@ -0,0 +1,19 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    seeFeed: async (_, _, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      const follwing = await prisma.user({id: user.id}).following();
+      return prisma.posts({
+        where: {
+          user: {
+            id_in: [...follwing.map(user => user.id), user.id]
+          }
+        },
+        orderBy: "caption_DESC"
+      })
+    }
+  }
+}
