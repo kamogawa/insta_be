9fc9310b89ca91bbd79603f985bf3450b7c69bcb
BEUMGOO <captain_kbg@naver.com>
2020-05-15 18:54:50 +0900

    add createdAt / bug fix

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -5,10 +5,15 @@ export default {
     createAccount: async(_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
       const exists = await prisma.$exists.user({
-        username
+        OR: [
+          {
+            username
+          },
+          { email }
+        ]
       });
       if (exists) {
-        throw Error("This username is already taken");
+        throw Error("This username / email is already taken");
       }
       await prisma.createUser({
         username, 
